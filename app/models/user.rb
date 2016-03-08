47acef4d6c5e57cb5a84ac1e5c1dd1aa4b9becdd
Robert Dean <rovo79@gmail.com>
2016-03-08 15:52:00 -0500

    added sendgrid, updated email config

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,14 @@ class User < ActiveRecord::Base
 validate :email_is_unique, on: :create
 after_create :create_account
 
-  def confirmation_required?
-    false
-  end
+  # def confirmation_required?
+  #   false
+  # end
 
   private
 #email should be unique in Account model
     def email_is_unique
+      #Do not validate email if errors are already present.
       return false unless self.errors[:email].empty?
 
       unless Account.find_by_email(email).nil?
---
+++ config/initializers/setup_email.rb
@@ -0,0 +1,24 @@
+if Rails.env.development?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'rovo-guide.herokuapp.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+elseif Rails.env.production?
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV["SENDGRID_PASSWORD"],
+    password:             ENV["SENDGRID_USERNAME"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+end
